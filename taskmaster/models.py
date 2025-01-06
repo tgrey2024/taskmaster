af32a0c1b5d3a50e4a6d7138d839efde55d7179b
Tripta Grey <tripta.grey@gmail.com>
2025-01-06 18:57:43 +0000

    create category and task models

--- taskmaster/models.py
+++ taskmaster/models.py
@@ -9,3 +9,18 @@ class LogMessage(models.Model):
         """Returns a string representation of a message."""
         date = timezone.localtime(self.log_date)
         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
