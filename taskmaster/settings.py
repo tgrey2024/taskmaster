68c490f6d5003e11ae134b7ec89b8aa18582313e
Tripta Grey <tripta.grey@gmail.com>
2025-01-06 15:30:52 +0000

    add db url

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from .env import SECRET_KEY, DATABASE_URL
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
