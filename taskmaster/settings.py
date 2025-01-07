b4c2d839f2f6871a61bbf82a0a499353dd074bf5
Tripta Grey <tripta.grey@gmail.com>
2025-01-07 15:54:41 +0000

    add crispyforms to add tasks

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
     """
@@ -7,12 +8,21 @@ def home(request):
     with the tasks due soonest at the top
     """
 
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+
     to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
     done_tasks = Task.objects.filter(completed=True).order_by('due_date')
 
     context = {
         'to_do_tasks': to_do_tasks,
         'done_tasks': done_tasks,
+        'form': form,
     }
 
     return render(request, 'tasks/index.html', context)
